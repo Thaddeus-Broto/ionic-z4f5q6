17276c16bf96739a8b3b69f8d99433f509c661cf
Thaddeus-Broto <thaddeus.broto150262@my.lsgh.edu.ph>
2020-09-21 14:00:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-z4f5q6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-z4f5q6)
---
+++ pages/about/about.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html',
+  styleUrls: ['about.css'],
+})
+export class AboutPage {
+
+  origPrice:number; 
+  sellPrice:number;
+  markdownSavings: number;
+  markdownRate: number;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+compute() {
+  this.markdownSavings = this.origPrice - this.sellPrice;
+  this.markdownRate = (this.markdownSavings / this.sellPrice) * 100;
+
+}
+
+}
